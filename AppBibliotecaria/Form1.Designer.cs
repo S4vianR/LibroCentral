5ed105c4e19af2d37e8b14840f55f35eaafceac9
S4vianR <reuben4.com@gmail.com>
2023-11-20 05:05:35 -0700

    Commit del incio de la segunda aplicacion "AppBibliotecaria"

--- AppBibliotecaria/Form1.Designer.cs
+++ AppBibliotecaria/Form1.Designer.cs
@@ -28,37 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.customLabel1 = new PracticaU2_Componentes.CustomLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // customLabel1
+            // button1
             // 
-            this.customLabel1.AutoSize = true;
-            this.customLabel1.CustomText = null;
-            this.customLabel1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold);
-            this.customLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(43)))));
-            this.customLabel1.Location = new System.Drawing.Point(254, 9);
-            this.customLabel1.Name = "customLabel1";
-            this.customLabel1.Size = new System.Drawing.Size(219, 24);
-            this.customLabel1.TabIndex = 0;
-            this.customLabel1.Text = "Aplicación Bibliotecaria";
+            this.button1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(315, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Prueba conexion";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 106);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 332);
+            this.dataGridView1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.customLabel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Aplicación Bibliotecaria";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private PracticaU2_Componentes.CustomLabel customLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- AppBibliotecaria/Form1.cs
+++ AppBibliotecaria/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InventarioMaster;
+using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,44 @@ namespace AppBibliotecaria
 {
     public partial class Form1 : Form
     {
+        // Variables
+        ConexionSQL conexion = new ConexionSQL();
+        MySqlConnection conexionBD = new MySqlConnection();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Variables
+            string query;
+
+            try
+            {
+                // Inicializar conexion
+                conexionBD = conexion.conectar();
+
+                // Query
+                query = "SELECT * FROM libros";
+
+                // Ejecutar query
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conexionBD))
+                {
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al ejecutar la consulta: " + ex.Message, "Error de consulta");
+            }
+            finally
+            {
+                // Cerrar conexion
+                conexion.cerrarConexion(conexionBD);
+            }
         }
     }
 }
